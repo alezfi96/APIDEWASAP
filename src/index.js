4d6fc86995b1bbdd6293f89ca2885b67cc6b2916
alezfi96 <alezfigueroa@hotmail.com>
2023-10-02 23:41:45 -0500

    apidewasap

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const apiruta = require('./routes/ruta');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+;
+
+app.use(express.json());
+app.use("/api",apiruta);
+
+
+app.listen(PORT,()=>{
+    console.log("El puerto de la API es :"+ PORT);
+})
+
